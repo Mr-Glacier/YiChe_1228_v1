05479ecf32b77562590873928e90b0c93a2936cc
Mr-Glacier <K15100415112@163.com>
2024-01-12 20:42:11 +0800

    主函数增加

--- src/main/java/org/example/Controller/Main_pic.java
+++ src/main/java/org/example/Controller/Main_pic.java
@@ -52,14 +52,53 @@ public class Main_pic {
         String version_save_path = "F:\\A_ZKZD_2024\\易车图片\\version\\";
         String in_version_path = version_save_path+"in_version\\";
         String out_version_path = version_save_path+"out_version\\";
+        HelpCreateFile.Method_Creat_folder(in_version_path);
+        HelpCreateFile.Method_Creat_folder(out_version_path);
+
 
 
 //        8.下载具有360图片的版本JSON
-        CP.Method_8_version_hase360(url_360,out_version_path,in_version_path);
+//        CP.Method_8_version_hase360(url_360,out_version_path,in_version_path);
+
+
+//        9.解析具有360图片的版本
+//        CP.Method_9_Analysis_hase36(out_version_path,in_version_path);
 
 
 
 
+        String in_version_path_pic_JSON =  version_save_path+"in_version_pic\\";
+        String out_version_path_pic_JSON =  version_save_path+"out_version_pic\\";
+        HelpCreateFile.Method_Creat_folder(in_version_path_pic_JSON);
+        HelpCreateFile.Method_Creat_folder(out_version_path_pic_JSON);
+
+//        10.下载内外360的数据
+//        CP.Method_10_Down_version360info(out_version_path_pic_JSON,in_version_path_pic_JSON);
+
+//        11.解析360图片地址
+//        CP.Method_11_Analysis_version360info(out_version_path_pic_JSON,in_version_path_pic_JSON);
+
+
+
+//        12.下载360 out 图片
+        String out_360_picSavePath = "F:\\A_ZKZD_2024\\易车图片\\version_360\\version_out_360\\";
+//        CP.Method_12_Down_360outPic(out_360_picSavePath);
+
+
+
+
+//        13.下载车型的版本
+        String model_version_Path = "F:\\A_ZKZD_2024\\易车图片\\model_all\\";
+
+//        CP.Method_13_Down_ModelHTML(model_version_Path);
+
+
+//        14.解析版本
+//        CP.Method_14_Analysis_ModelHTML(model_version_Path);
+
+//        15.下载
+        String firsthtml = "F:\\A_ZKZD_2024\\易车图片\\Pic_外观_前排_后排\\model_firstHTML\\";
+        CP.Method_15_Down_外观_前排_后排(firsthtml);
 
 
 //        测试图片版本接口
@@ -69,7 +108,5 @@ public class Main_pic {
 //        int CID = 601;
 //        String content = CP.Method_RequestConfigAPI(ModelID,versionurl,"12",CID);
 //        System.out.println(content);
-
     }
-
 }
