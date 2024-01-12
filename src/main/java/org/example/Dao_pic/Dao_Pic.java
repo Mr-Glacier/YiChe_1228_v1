437d28f284c51e518e719a0bbf0ada4937580172
Mr-Glacier <K15100415112@163.com>
2024-01-12 20:40:50 +0800

    增加dao

---
+++ src/main/java/org/example/Dao_pic/Dao_Pic.java
@@ -0,0 +1,17 @@
+package org.example.Dao_pic;
+
+import org.example.Dao.DaoFather;
+
+public class Dao_Pic extends DaoFather {
+    public Dao_Pic(int choseDB, int choseTable) {
+        super(choseDB, choseTable);
+    }
+
+//    这个类用于下载各种的图片
+
+
+
+
+
+
+}
